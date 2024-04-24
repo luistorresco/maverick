4c0761da3c90925fa80c0686a55fb8f7d64dec75
Luis Fernando Torres Correa <luistorresco@gmail.com>
2024-04-24 18:54:46 -0500

    Update db.py
    fix

--- config/db.py
+++ config/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://u317228138_strores:~SpDKMl>P5@localhost:3306/u317228138_strores")
+engine = create_engine("mysql+pymysql://u317228138_strores:Ukv52Knoy6U&@62.72.50.52/u317228138_strores")
 
 meta = MetaData()
 conn = engine.connect()
